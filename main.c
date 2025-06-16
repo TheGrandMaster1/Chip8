715598c3b86874e0a2d9adf39dce92559a8b644f
Lena Bousa <cookinglove27@gmail.com>
2025-06-16 13:58:21 +0100

    tested the initialize function

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "chip8.c"
 
 int main(){
-    
+    CHIP8 chip8;
+    chip8_initialize(&chip8);
 
+    for (int i = 0; i < 16; i++) {
+        printf("V[%X] = %d\n", i, chip8.register_V[i]);
+    }
+    printf("PC = 0x%X\n", chip8.pc);
  
 }
