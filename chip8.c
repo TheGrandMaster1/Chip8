f4e8d5711cc54c34c57a99a60aad76b595189741
Lena Bousa <cookinglove27@gmail.com>
2025-06-16 13:36:27 +0100

    added an initialization function

--- chip8.c
+++ chip8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef struct{
+typedef struct chip_8{
     uint8_t memory[4096]; //4K byte-addressable memory
     uint8_t register_V[16]; //16 8-bit general purpose registers V0 to VF
     uint16_t register_I; //16-bit index register
@@ -11,10 +11,13 @@ typedef struct{
     uint8_t sp; //1 byte stack pointer
     uint8_t delay_timer;
     uint8_t sound_timer;
-    uint8_t keyboard[16]; // 4x4 hexadecimal keyboard
-}CHIP8;
+    uint8_t keyboard; // 4x4 hexadecimal keyboard
+} CHIP8;
 
+const unsigned int START_ADDRESS = 0x200;
 
-int main() {
-   
+void chip8_initialize(CHIP8* chip8){
+    memset(chip8, 0, sizeof(CHIP8)); // sets all the structure a\members to 0
+    chip8->pc = START_ADDRESS;
+    
 }
